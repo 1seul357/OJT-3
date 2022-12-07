0f29af00e28fb5085bf8cadb2f533fb62ac900d8
1seul357 <1seul357@gmail.com>
2022-12-07 10:30:17 +0900

    feat: 그룹화 크기 변경 기능 추가

--- src/utils/clickGroup.ts
+++ src/utils/clickGroup.ts
@@ -1,8 +1,14 @@
 import { Container, Shape, Svg } from "@svgdotjs/svg.js";
 import { removeSelector } from "./removeSelector";
+import { removeShapeGroup } from "./removeShape";
 import "../css/clickItem.css";
 
-export const clickGroup = (group: Container, draw: Svg, select: Shape) => {
+export const clickGroup = (
+  group: Container,
+  draw: Svg,
+  select: Shape,
+  setGroup: Function
+) => {
   document.querySelector(".g")?.remove();
   const g = draw.group();
   g.add(group).fill("transparent").stroke("#66666699").addClass("g");
@@ -34,8 +40,10 @@ export const clickGroup = (group: Container, draw: Svg, select: Shape) => {
 
   const makeController = () => {
     document.querySelector(".gclone")?.remove();
-    const clone = select
-      .clone()
+    const clone = draw
+      .rect(Number(group.width()), Number(group.height()))
+      .x(group.x())
+      .y(group.y())
       .stroke("#66666699")
       .addClass("gclone")
       .fill("transparent")
@@ -61,7 +69,7 @@ export const clickGroup = (group: Container, draw: Svg, select: Shape) => {
     const rotate = group
       .circle(20)
       .cx(cx)
-      .cy(cy - Number(select.height()) / 1.4)
+      .cy(cy - Number(clone.height()) / 1.2)
       .addClass("rotate")
       .attr({ fill: "#CCCCFF" })
       .transform(group.transform())
@@ -72,7 +80,7 @@ export const clickGroup = (group: Container, draw: Svg, select: Shape) => {
       rotate.hide();
       const moveHandler = (e: MouseEvent) => {
         e.preventDefault();
-        // circles.forEach((el) => el.remove());
+        circles.forEach((el) => el.remove());
 
         const pt = draw.point(e.clientX, e.clientY);
         const x = x1 + Number(group.width());
@@ -90,7 +98,7 @@ export const clickGroup = (group: Container, draw: Svg, select: Shape) => {
         rotate.show();
         rotate
           .cx(group.cx())
-          .cy(group.cy() - Number(select.height()) / 1.4)
+          .cy(group.cy() - Number(clone.height()) / 1.2)
           .transform(group.transform());
         draw.off("mousemove", moveHandler as EventListener);
         draw.off("mouseup", upHandler);
@@ -98,11 +106,72 @@ export const clickGroup = (group: Container, draw: Svg, select: Shape) => {
       draw.on("mousemove", moveHandler as EventListener);
       draw.on("mouseup", upHandler);
     });
+
+    const circles = pts.map((pt, i) => {
+      const circle = g
+        .circle(10)
+        .cx(pt[0])
+        .cy(pt[1])
+        .addClass("circles")
+        .transform(group.transform())
+        .fill("#666666")
+        .mousedown((e: MouseEvent) => {
+          document.querySelector(".rotate")?.remove();
+          e.stopPropagation();
+          const moveHandler = (e: MouseEvent) => {
+            const point = draw.point(e.clientX, e.clientY);
+            const rotatedPoint = point.transform(group.matrix().inverse());
+            const dx = rotatedPoint.x - pt[0];
+            const dy = rotatedPoint.y - pt[1];
+
+            if (i === 0) {
+              clone
+                .width(Number(group.width()) - dx)
+                .height(Number(group.height()) - dy)
+                .x(Number(group.x()) + dx)
+                .y(Number(group.y()) + dy);
+            }
+            if (i === 1) {
+              clone
+                .width(Number(group.width()) + dx)
+                .height(Number(group.height()) - dy)
+                .x(group.x())
+                .y(Number(group.y()) + dy);
+            }
+            if (i === 2) {
+              clone
+                .width(Number(group.width()) - dx)
+                .height(Number(group.height()) + dy)
+                .x(Number(group.x()) + dx)
+                .y(group.y());
+            }
+            if (i === 3) {
+              clone
+                .width(Number(group.width()) + dx)
+                .height(Number(group.height()) + dy)
+                .x(group.x())
+                .y(group.y());
+            }
+          };
+          const upHandler = () => {
+            group.size(clone.width(), clone.height()).x(clone.x()).y(clone.y());
+            remove();
+            draw.off("mousemove", moveHandler as EventListener);
+            draw.off("mouseup", upHandler);
+            controller = makeController();
+          };
+          draw.on("mousemove", moveHandler as EventListener);
+          draw.on("mouseup", upHandler);
+        });
+      return circle;
+    });
     const remove = () => {
+      circles.forEach((el) => el.remove());
       clone.remove();
       rotate.remove();
     };
     return remove;
   };
+  removeShapeGroup(group, setGroup);
   controller = makeController();
 };
